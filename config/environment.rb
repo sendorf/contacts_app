d07aacd1b24cfe35d40e3434247d09d7e9428c44
Sergio Díaz <sendorf@gmail.com>
2014-03-30 13:00:38 +0200

    Created the rails application

---
+++ config/environment.rb
@@ -0,0 +1,9 @@
+# Load the rails application
+require File.expand_path('../application', __FILE__)
+
+# Load alwaysdata vars from local file
+alwaysdata_env = File.join(Rails.root, 'config', 'alwaysdata_env.rb')
+load(alwaysdata_env) if File.exists?(alwaysdata_env)
+
+# Initialize the rails application
+ContactsApp::Application.initialize!
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ContactsApp::Application.config.secret_token = '6bebbb459a547f009a767ae42678f8c8dae04ff5e90d38587325f2d6741ae15835cbb2f9f21e9ad29f0dcc378869afe61ea4ce9dc967a5da723c67bfd04df210'
