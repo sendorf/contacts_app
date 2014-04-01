5cb6917197c4bcbd8997b1280c6fab28bcc6fad5
Sergio Díaz <sendorf@gmail.com>
2014-04-01 08:38:55 +0200

    Created the view for each contact and for editing it, but the controller does not update the contact yet

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -7,5 +7,22 @@ class ContactsController < ApplicationController
 	def new
 		@contact = Contact.new
 	end
-	
+
+	def create
+		@contact = Contact.new(params[:contact])
+		if @contact.save
+			redirect_to @contact
+		else
+			redirect_to new_server_path(@contact)
+		end
+	end
+
+	def show
+		@contact = Contact.find(params[:id])
+	end
+
+	def edit
+		@contact = Contact.find(params[:id])
+	end
+
 end
